3c6beb1233f7164a7c2337c51d917239b183b9a5
ZakyMalika <zakymalika0987@gmail.com>
2026-01-02 19:18:53 +0700

    membuat isi file ContainerApp.kt

--- app/src/main/java/com/example/nosql/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/nosql/repositori/ContainerApp.kt
@@ -1,1 +1,23 @@
 package com.example.nosql.repositori
+
+import android.app.Application
+
+interface ContainerApp{
+    val repositorySiswa: RepositorySiswa
+
+}
+
+class DefaultAppContainer: ContainerApp{
+    override val repositorySiswa: RepositorySiswa by lazy {
+        FirebaseRepositorySiswa()
+    }
+}
+
+class AplikasiDataSiswa: Application(){
+    lateinit var container: ContainerApp
+    override fun onCreate() {
+        super.onCreate()
+        container = DefaultAppContainer()
+    }
+}
+
