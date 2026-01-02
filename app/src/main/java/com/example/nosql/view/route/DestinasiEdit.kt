726e6dbab5131394b9b977a8bbaaaefae48a8f2b
ZakyMalika <zakymalika0987@gmail.com>
2026-01-02 19:20:26 +0700

    membuat isi  file DestinasiEdit.kt

--- app/src/main/java/com/example/nosql/view/route/DestinasiEdit.kt
+++ app/src/main/java/com/example/nosql/view/route/DestinasiEdit.kt
@@ -2,3 +2,10 @@ package com.example.nosql.view.route
 
 import com.example.nosql.R
 
+object DestinasiEdit: DestinasiNavigasi {
+    override val route = "item_edit"
+    override val titleRes = R.string.edit_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
+
