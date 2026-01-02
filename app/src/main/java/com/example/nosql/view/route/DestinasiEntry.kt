c8a52c096dfe06dcf245efcd19600670fc81b8a8
ZakyMalika <zakymalika0987@gmail.com>
2026-01-02 19:21:03 +0700

    membuat isi file DestinasiEntry.kt

--- app/src/main/java/com/example/nosql/view/route/DestinasiEntry.kt
+++ app/src/main/java/com/example/nosql/view/route/DestinasiEntry.kt
@@ -1,3 +1,8 @@
 package com.example.nosql.view.route
 
 import com.example.nosql.R
+
+object DestinasiEntry: DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
